b30261d818c17b1525c9662cc31f0621fb50a026
ikar365 <narioze@gmail.com>
2024-07-16 01:40:29 +0200

    Update NBuf.js

--- libs/NBuf.js
+++ libs/NBuf.js
@@ -1,28 +1,26 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NBuf2 = exports.NBuf3 = void 0;
-var NBuf3 = /** @class */ (function () {
-    function NBuf3(ct) {
+class NBuf3 {
+    constructor(ct) {
         this.top = 0;
         this.array = new Float32Array(ct);
     }
-    NBuf3.prototype.write = function (v) {
+
+    write(v) {
         this.array[this.top++] = v.x;
         this.array[this.top++] = v.y;
         this.array[this.top++] = v.z;
-    };
-    return NBuf3;
-}());
-exports.NBuf3 = NBuf3;
-var NBuf2 = /** @class */ (function () {
-    function NBuf2(ct) {
+    }
+}
+
+class NBuf2 {
+    constructor(ct) {
         this.top = 0;
         this.array = new Float32Array(ct);
     }
-    NBuf2.prototype.write = function (v) {
+
+    write(v) {
         this.array[this.top++] = v.x;
         this.array[this.top++] = v.y;
-    };
-    return NBuf2;
-}());
-exports.NBuf2 = NBuf2;
+    }
+}
+
+export { NBuf3, NBuf2 };
