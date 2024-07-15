ac8b530f23cfaac9a476a693418e0bb262c42c2d
ikar365 <narioze@gmail.com>
2024-07-16 01:46:47 +0200

    Update Vertex.js

--- libs/Vertex.js
+++ libs/Vertex.js
@@ -1,7 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Vertex = void 0;
-var Vector_1 = require("./Vector");
+import { Vector } from './Vector.js';
+
 /**
  * Represents a vertex of a polygon. Use your own vertex class instead of this
  * one to provide additional features like texture coordinates and vertex
@@ -11,28 +9,37 @@ var Vector_1 = require("./Vector");
  * functions like `CSG.sphere()` can return a smooth vertex normal, but `normal`
  * is not used anywhere else.
  */
-var Vertex = /** @class */ (function () {
-    function Vertex(pos, normal, uv, color) {
-        this.pos = new Vector_1.Vector().copy(pos);
-        this.normal = new Vector_1.Vector().copy(normal);
-        this.uv = new Vector_1.Vector().copy(uv);
+class Vertex {
+    constructor(pos, normal, uv, color) {
+        this.pos = new Vector().copy(pos);
+        this.normal = new Vector().copy(normal);
+        this.uv = new Vector().copy(uv);
         this.uv.z = 0;
-        color && (this.color = new Vector_1.Vector().copy(color));
+        color && (this.color = new Vector().copy(color));
     }
-    Vertex.prototype.clone = function () {
+
+    clone() {
         return new Vertex(this.pos, this.normal, this.uv, this.color);
-    };
+    }
+
     // Invert all orientation-specific data (e.g. vertex normal). Called when the
     // orientation of a polygon is flipped.
-    Vertex.prototype.flip = function () {
+    flip() {
         this.normal.negate();
-    };
+    }
+
     // Create a new vertex between this vertex and `other` by linearly
     // interpolating all properties using a parameter of `t`. Subclasses should
     // override this to interpolate additional properties.
-    Vertex.prototype.interpolate = function (other, t) {
-        return new Vertex(this.pos.clone().lerp(other.pos, t), this.normal.clone().lerp(other.normal, t), this.uv.clone().lerp(other.uv, t), this.color && other.color && this.color.clone().lerp(other.color, t));
-    };
-    return Vertex;
-}());
-exports.Vertex = Vertex;
+    interpolate(other, t) {
+        return new Vertex(
+            this.pos.clone().lerp(other.pos, t),
+            this.normal.clone().lerp(other.normal, t),
+            this.uv.clone().lerp(other.uv, t),
+            this.color && other.color && this.color.clone().lerp(other.color, t)
+        );
+    }
+}
+
+export { Vertex };
+
