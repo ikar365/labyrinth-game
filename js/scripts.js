e0d1e221053b892a86cbc3343f1e78ce53020c72
ikar365 <narioze@gmail.com>
2024-07-16 00:26:16 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-import * as THREE from "../node_modules/three/build/three.module.js;
+import * as THREE from '../libs/three.module.js';
 import { CSG } from '../libs/three-csg.js';
 
 let scene, camera, renderer, blueBall, orangeBall;
