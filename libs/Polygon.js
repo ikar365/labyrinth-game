d8a9e616be7a7be8defd693f34f1c29470b2cc67
ikar365 <narioze@gmail.com>
2024-07-16 01:43:42 +0200

    Update Polygon.js

--- libs/Polygon.js
+++ libs/Polygon.js
@@ -1,7 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Polygon = void 0;
-var Plane_1 = require("./Plane");
+import { Plane } from './Plane.js';
+
 /**
  * Represents a convex polygon. The vertices used to initialize a polygon must
  * be coplanar and form a convex loop. They do not have to be `Vertex`
@@ -12,19 +10,22 @@ var Plane_1 = require("./Plane");
  * polygons that are clones of each other or were split from the same polygon.
  * This can be used to define per-polygon properties (such as surface color).
  */
-var Polygon = /** @class */ (function () {
-    function Polygon(vertices, shared) {
+class Polygon {
+    constructor(vertices, shared) {
         this.vertices = vertices;
         this.shared = shared;
-        this.plane = Plane_1.Plane.fromPoints(vertices[0].pos, vertices[1].pos, vertices[2].pos);
+        this.plane = Plane.fromPoints(vertices[0].pos, vertices[1].pos, vertices[2].pos);
     }
-    Polygon.prototype.clone = function () {
-        return new Polygon(this.vertices.map(function (v) { return v.clone(); }), this.shared);
-    };
-    Polygon.prototype.flip = function () {
-        this.vertices.reverse().map(function (v) { return v.flip(); });
+
+    clone() {
+        return new Polygon(this.vertices.map((v) => v.clone()), this.shared);
+    }
+
+    flip() {
+        this.vertices.reverse().map((v) => v.flip());
         this.plane.flip();
-    };
-    return Polygon;
-}());
-exports.Polygon = Polygon;
+    }
+}
+
+export { Polygon };
+
