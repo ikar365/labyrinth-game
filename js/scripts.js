22b188eb53a9fa30f3e550092561ffb2d0a04593
ikar365 <narioze@gmail.com>
2024-07-16 22:51:54 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ import { CSG } from '../libs/CSG.js';
 let scene, camera, renderer, controls;
 let player, enemy, redBalls = [];
 let playerSpeed = 0.9;
-let enemySpeed = playerSpeed * 0.5; // Slowed down by 50 percent
+let enemySpeed = playerSpeed * 0.34; // 34% of playerSpeed
 let redBallSpeed = playerSpeed * 1.25;
 let redBallSpawnTime = 15000; // 15 seconds
 let lastRedBallSpawnTime = 0;
